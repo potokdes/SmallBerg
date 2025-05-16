93478238cd46a201079edd7887dcea2e7426a5a5
Piotr Mierzejewski <potokdes@gmail.com>
2025-05-16 16:09:13 +0000

    Fix(Table) Removed the method get_last_row it was just for testing.

--- tests/test_Table.cpp
+++ tests/test_Table.cpp
@@ -31,17 +31,3 @@ TEST_CASE("Table should return the copy of the schema for viewing if requested")
     REQUIRE_THROWS(s.get_column("test_col2"));
    
 }
-
-TEST_CASE("Adding rows to table")
-{
-    auto v = std::vector<std::string>();
-    v.push_back("A");
-    v.push_back("B");
-    v.push_back("C");
-    v.push_back("D");
-    std::string s = v.back();
-    Table t("test_table");
-    t.append_row(v);
-
-    REQUIRE(v == t.get_last_row()); // basic sanity check, expand this!
-}
